5f3e57f43330013cdb772bc21d2712db444104eb
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-02-07 00:46:38 +0700

    Add Touch, Screen Res, Shoot Barrier

--- Prototype01/Assets/Scripts/Player.cs
+++ Prototype01/Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	private Rigidbody2D playerBody;
 	public float maxSpeed;
+	public Transform shootCollider;
+	public float shootSpeed;
+	public float cooldownTime;
+	private bool allowShoot = true;
 	// Use this for initialization
 	void Start () {
 		playerBody = gameObject.GetComponent<Rigidbody2D>();
@@ -13,8 +17,12 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		float horizontalSpeed;
-		horizontalSpeed = inputHorizontal();
+		//horizontalSpeed = inputHorizontal();
+		horizontalSpeed = inputTouchHorizontal();
 		MoveHorizontal(horizontalSpeed);
+		if (inputTouchShoot()) {
+			Shoot();
+		}
 	}
 
 	public void MoveHorizontal(float horizontalSpeed) {
@@ -23,5 +31,31 @@ public class Player : MonoBehaviour {
 
 	float inputHorizontal() {
 		return (Input.GetAxis ("Horizontal"));
+	}
+	float inputTouchHorizontal() {
+		if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Moved)) {
+			return(Input.GetTouch(0).deltaPosition.x);
+		} else {
+			return (0f);
+		}
+	}
+	bool inputTouchShoot() {
+		if ((Input.touchCount > 1) && (Input.GetTouch(1).phase == TouchPhase.Began)) {
+			return (true);
+		} else {
+			return (false);
+		}
+	}
+	void Shoot() {
+		if (allowShoot) {
+			allowShoot = false;
+			shootCollider.localScale = new Vector2(shootCollider.localScale.x*shootSpeed,shootCollider.localScale.y*shootSpeed);
+			StartCoroutine(Cooldown());
+		}
+	}
+	IEnumerator Cooldown() {
+		yield return new WaitForSeconds(cooldownTime);
+		shootCollider.localScale = new Vector2(1f,1f);
+		allowShoot = true;
 	}
 }
